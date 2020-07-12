96e9598a6c4133ec8f0607da5943313af1ad787c
Jose Campos <josigna.cp@gmail.com>
2020-07-12 00:43:57 -0400

    se utiliza fork para crear proceso hijo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SO-Laboratorio1
+Ejecutar:
+1) $ make
+2) $ ./salida -c 2 -m mascara.txt -u 120 -n 3 -b
--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include "leerArgs.h"
 #include "Image.h"
 #include "utils.h"
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+Image LeerImagen(int num, char *ruta);
+Image ConvertirAGris(Image *orig, int i, char *ruta);
+void FiltroLapleciano(const Image *orig, int i, char *ruta, char *fnameLaplaciana);
+void Binarizacion(Image *orig, int i, char *ruta, int umbral);
 
 
 // ejecutar: ./salida -c 2 -m test -u 120 -n 3 -b
@@ -21,76 +29,31 @@ int main(int argc, char *argv[]) {
     // kernel = leerArchivo(maskLaplaciana);
     // printf("%d\n", kernel[1][1]);
 
+    pid_t nuevoProceso;
 
-    for (int i = 0; i < cantImgs; ++i) {
+    if ((nuevoProceso = fork()) == 0){
+        printf("Soy el hijo y voy a procesar las imagenes. Proceso Id de mi hijo es:%d \n", nuevoProceso);
 
-        char fileNameOrigen[50];
-        char fileNameDest[50];
+        for (int i = 0; i < cantImgs; i++)
+        {
+            Image img_original;
+            img_original = LeerImagen(i, ruta);
 
-        sprintf(fileNameOrigen,"imagen_%d.jpg", i);
+            img_original = ConvertirAGris(&img_original, i, ruta);
 
-        char *rutaCompleta = concatenar(ruta, fileNameOrigen);
+            FiltroLapleciano(&img_original, i, ruta, maskLaplaciana);
 
-        //printf("%s -> procesando\n" , rutaCompleta);
+            Binarizacion(&img_original, i, ruta, uBinarImg);
+        }
+        
+        exit(0);
 
-
-        Image img_original;
-
-
-        Image_load(&img_original, rutaCompleta);
-        //ON_ERROR_EXIT(img_original.data == NULL, "Error al cargar imagen");
-
-        //printf("%s -> Imagen cargada\n" , rutaCompleta);
-
-        // Convertir imagen a escala de grises
-        Image img_gris;
-        Image_to_gray(&img_original, &img_gris);
-
-        //printf("%s -> Imagen convertida a gris\n" , rutaCompleta);
-
-        // Guardar imagenes
-        sprintf(fileNameDest,"imagen_%d_gris.jpg", i);
-        char *rutaCompletaDest = concatenar(ruta, fileNameDest);
-        Image_save(&img_gris, rutaCompletaDest);
-
-        //printf("%s -> Imagen gris guardando en: %s\n" , rutaCompleta, rutaCompletaDest);
-
-        // Filtro lapleciano
-        Image img_lapleciano;
-        Image_lapleciano(&img_gris, &img_lapleciano, maskLaplaciana);
-        //printf("%s -> Imagen convertida a lapleciano\n" , rutaCompleta);
-
-        sprintf(fileNameDest,"imagen_%d_lapleciano.jpg", i);
-        rutaCompletaDest = concatenar(ruta, fileNameDest);
-        Image_save(&img_lapleciano, rutaCompletaDest);
-
-        //printf("%s -> Imagen gris guardando en: %s\n" , rutaCompleta, rutaCompletaDest);
-
-        // Fin Filtro Lapleciano
+    } else {
+        printf("Soy el padre y ya lei los parametros. El proceso Hijo Id es:%d \n", nuevoProceso);
+    }
         
 
-        // Binarizando la imagen
-        Image img_binary;
-        //Image_to_binary(&img_original, &img_binary, uBinarImg);
-        Image_to_binary_from_gray(&img_gris, &img_binary, uBinarImg); // binarizado por la escala de grises
-
-
-        //printf("%s -> Imagen binarizada\n" , rutaCompleta);
-
-    
-        sprintf(fileNameDest,"imagen_%d_binary.jpg", i);
-        rutaCompletaDest = concatenar(ruta, fileNameDest);
-        Image_save(&img_binary, rutaCompletaDest);
-
-        //printf("%s -> Imagen binarizada guardando en: %s\n" , rutaCompleta, rutaCompletaDest);
-
-
-        // Liberar memoria
-        Image_free(&img_original);
-        Image_free(&img_gris);
-        Image_free(&img_binary);
-
-    }
+    //exit(0);
 
     // muestra tabla de nearly black
     if (mostrarResult == 1)
@@ -125,5 +88,65 @@ int main(int argc, char *argv[]) {
 }
 
 
+Image LeerImagen(int num, char *ruta){
+    char fileNameOrigen[50];
+
+    sprintf(fileNameOrigen,"imagen_%d.jpg", num);
+
+    char *rutaCompleta = concatenar(ruta, fileNameOrigen);
+
+    //printf("%s -> procesando\n" , rutaCompleta);
+
+    Image tmp;
+
+    Image_load(&tmp, rutaCompleta);
+
+    return tmp;
+}
+
+Image ConvertirAGris(Image *orig, int i, char *ruta){
+    char fileNameDest[50];
+    Image img_gris;
+    Image_to_gray(orig, &img_gris);
+
+    //printf("%s -> Imagen convertida a gris\n" , rutaCompleta);
+
+    // Guardar imagenes
+    sprintf(fileNameDest,"imagen_%d_gris.jpg", i);
+    char *rutaCompletaDest = concatenar(ruta, fileNameDest);
+    Image_save(&img_gris, rutaCompletaDest);
+
+    return img_gris;
+}
+
+void FiltroLapleciano(const Image *orig, int i, char *ruta, char *fnameLaplaciana){
+    char fileNameDest[50];
+    
+    // Filtro lapleciano
+    Image img_lapleciano;
+    Image_lapleciano(orig, &img_lapleciano, fnameLaplaciana);
+    //printf("%s -> Imagen convertida a lapleciano\n" , rutaCompleta);
+
+    sprintf(fileNameDest,"imagen_%d_lapleciano.jpg", i);
+    char *rutaCompletaDest = concatenar(ruta, fileNameDest);
+    Image_save(&img_lapleciano, rutaCompletaDest);
+}
+
+void Binarizacion(Image *orig, int i, char *ruta, int umbral){
+    char fileNameDest[50];
+    // Binarizando la imagen
+    Image img_binary;
+    //Image_to_binary(&img_original, &img_binary, uBinarImg);
+    Image_to_binary_from_gray(orig, &img_binary, umbral); // binarizado por la escala de grises
 
 
+    //printf("%s -> Imagen binarizada\n" , rutaCompleta);
+
+
+    sprintf(fileNameDest,"imagen_%d_binary.jpg", i);
+    char *rutaCompletaDest = concatenar(ruta, fileNameDest);
+    Image_save(&img_binary, rutaCompletaDest);
+
+    //printf("%s -> Imagen binarizada guardando en: %s\n" , rutaCompleta, rutaCompletaDest);
+
+}
